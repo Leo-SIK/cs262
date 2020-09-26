44dc588dc36bd3e9dbc57dc4350c9effbbaaf065
Leo-SIK <05rt06@gmail.com>
2020-09-25 21:36:25 -0400

    Lab4

---
+++ lab04/gamezone/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import { AppLoading } from 'expo';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Navigator from './routes/homeStack'
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  
+  if(fontsLoaded) {
+    return (
+      <NavigationContainer>
+          <Navigator />
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <AppLoading
+      startAsync={getFonts}
+      onFinish={() => setFontsLoaded(true)}
+    />
+    )
+  }
+};
